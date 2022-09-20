c27b8d7c944fa311e31751c9cc376babccee0fa6
muzaim <muzaim.surya@yahoo.co.id>
2022-09-20 21:06:49 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
-// import "./script";
-import screenfull from "screenfull";
-import { useEffect } from "react";
+import { cobaMiring } from "./script";
 
 function App() {
-  useEffect(() => {
-    if (screenfull.isEnabled) {
-      screenfull.request();
-    }
-  });
   return (
-    <div className="App">
-      <div id="container">
-        <p id="orientation-status"></p>
-        <div id="buttons-container">
-          <button id="lock-landscape-button">Lock to Landscape Mode</button>
-          <button id="unlock-button">Unlock</button>
-        </div>
+    <div id="container">
+      <p id="orientation-status"></p>
+      <div id="buttons-container">
+        <button id="lock-landscape-button" onClick={cobaMiring}>
+          Click To Play
+        </button>
+        <button id="unlock-button">Unlock</button>
       </div>
     </div>
   );
--- src/script.js
+++ src/script.js
@@ -1,49 +1,47 @@
-var _LOCK_BUTTON = document.querySelector("#lock-landscape-button"),
-  _UNLOCK_BUTTON = document.querySelector("#unlock-button"),
-  _STATUS = document.querySelector("#orientation-status");
+// var _LOCK_BUTTON = document.querySelector("#lock-landscape-button"),
+//   _UNLOCK_BUTTON = document.querySelector("#unlock-button"),
+//   _STATUS = document.querySelector("#orientation-status");
 
-_STATUS.innerHTML = window.screen.orientation.type + " mode";
+// // upon lock to landscape-primary mode
+// _LOCK_BUTTON.addEventListener("click", function () {});
 
-// upon lock to landscape-primary mode
-document
-  .querySelector("#lock-landscape-button")
-  .addEventListener("click", function () {
-    if (document.documentElement.requestFullscreen)
-      document.querySelector("#container").requestFullscreen();
-    else if (document.documentElement.webkitRequestFullScreen)
-      document.querySelector("#container").webkitRequestFullScreen();
+export const cobaMiring = () => {
+  if (document.documentElement.requestFullscreen)
+    document.querySelector("#container").requestFullscreen();
+  else if (document.documentElement.webkitRequestFullScreen)
+    document.querySelector("#container").webkitRequestFullScreen();
 
-    window.screen.orientation
-      .lock("landscape-primary")
-      .then(function () {
-        _LOCK_BUTTON.style.display = "none";
-        _UNLOCK_BUTTON.style.display = "block";
-      })
-      .catch(function (error) {
-        alert(error);
-      });
-  });
+  // IKI penting brow
+  window.screen.orientation.lock("landscape-primary");
+  // .then(function () {
+  //   _LOCK_BUTTON.style.display = "none";
+  //   _UNLOCK_BUTTON.style.display = "block";
+  // })
+  // .catch(function (error) {
+  //   alert(error);
+  // });
+};
 
 // upon unlock
-document.querySelector("#unlock-button").addEventListener("click", function () {
-  window.screen.orientation.unlock();
+// document.querySelector("#unlock-button").addEventListener("click", function () {
+//   window.screen.orientation.unlock();
 
-  _LOCK_BUTTON.style.display = "block";
-  _UNLOCK_BUTTON.style.display = "none";
-});
+//   _LOCK_BUTTON.style.display = "block";
+//   _UNLOCK_BUTTON.style.display = "none";
+// });
 
 // when screen orientation changes
-window.screen.orientation.addEventListener("change", function () {
-  _STATUS.innerHTML = window.screen.orientation.type + " mode";
-});
+// window.screen.orientation.addEventListener("change", function () {
+//   _STATUS.innerHTML = window.screen.orientation.type + " mode";
+// });
 
 // on exiting full-screen lock is automatically released
-document.addEventListener("fullscreenchange", function () {
-  _LOCK_BUTTON.style.display = "block";
-  _UNLOCK_BUTTON.style.display = "none";
-});
+// document.addEventListener("fullscreenchange", function () {
+//   _LOCK_BUTTON.style.display = "block";
+//   _UNLOCK_BUTTON.style.display = "none";
+// });
 
-document.addEventListener("webkitfullscreenchange", function () {
-  _LOCK_BUTTON.style.display = "block";
-  _UNLOCK_BUTTON.style.display = "none";
-});
+// document.addEventListener("webkitfullscreenchange", function () {
+//   _LOCK_BUTTON.style.display = "block";
+//   _UNLOCK_BUTTON.style.display = "none";
+// });
